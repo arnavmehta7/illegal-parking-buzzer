0693ace9d04bb6398186a4de74e00d2ce1f6c5f4
arnavmehta7 <65492948+arnavmehta7@users.noreply.github.com>
2021-12-18 12:45:03 +0530

    cleaned things

--- script.js
+++ script.js
@@ -1,84 +1,86 @@
 console.log('attached');
+
+// the link of model file
 const URL = "https://teachablemachine.withgoogle.com/models/K4JuZnTWX/";
 
 let model, webcam, labelContainer, maxPredictions;
 
-
+// wake up audio is the place where i have stored my audio file
 var aud = document.getElementById("wakeUpAudio");
-function playAud() { 
-   aud.play(); 
- }
-function pauseAud() { 
-   aud.pause(); 
- }
+function playAud() {
+  aud.play();
+}
+function pauseAud() {
+  aud.pause();
+}
 
 
 async function init() {
-     
-        const modelURL = URL + "model.json";
-        const metadataURL = URL + "metadata.json";
+
+  const modelURL = URL + "model.json";
+  const metadataURL = URL + "metadata.json";
 
 
-        // load the model and metadata
-        // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
-        // or files from your local hard drive
-        // Note: the pose library adds "tmImage" object to your window (window.tmImage)
-        model = await tmImage.load(modelURL, metadataURL);
-        maxPredictions = model.getTotalClasses();
+  // load the model and metadata
+  // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
+  // or files from your local hard drive
+  // Note: the pose library adds "tmImage" object to your window (window.tmImage)
+  model = await tmImage.load(modelURL, metadataURL);
+  maxPredictions = model.getTotalClasses();
 
-        // Convenience function to setup a webcam
-        const flip = true; // whether to flip the webcam
-        webcam = new tmImage.Webcam(600,500, flip); // width, height, flip
-        await webcam.setup({ facingMode: "environment" }); // request access to the webcam
-        await webcam.play();
-        window.requestAnimationFrame(loop);
+  // Convenience function to setup a webcam
+  const flip = true; // whether to flip the webcam
+  webcam = new tmImage.Webcam(600, 500, flip); // width, height, flip
+  await webcam.setup({ facingMode: "enviroment" }); // request access to the webcam
+  await webcam.play();
+  window.requestAnimationFrame(loop);
 
-        // append elements to the DOM
-        document.getElementById("webcam-container").appendChild(webcam.canvas);
-        labelContainer = document.getElementById("label-container");
-        for (let i = 0; i < maxPredictions; i++) { // and class labels
-            labelContainer.appendChild(document.createElement("div"));
-        }
-      
+  // append elements to the DOM
+  document.getElementById("webcam-container").appendChild(webcam.canvas);
+  labelContainer = document.getElementById("label-container");
+  for (let i = 0; i < maxPredictions; i++) { // and class labels
+    labelContainer.appendChild(document.createElement("div"));
   }
 
+}
 
-  async function loop() {
-      webcam.update(); // update the webcam frame
 
-      await predict();
-      window.requestAnimationFrame(loop);
+async function loop() {
+  webcam.update(); // update the webcam frame
+
+  await predict();
+  window.requestAnimationFrame(loop);
+}
+
+// run the webcam image through the image model
+async function predict() {
+  // predict can take in an image, video or canvas html element
+  // console.log('Taking a break...');
+  // await sleep(4000);
+  // console.log('Two seconds later, showing sleep in a loop...');
+  const prediction = await model.predict(webcam.canvas);
+  for (let i = 0; i < maxPredictions; i++) {
+    const classPrediction =
+      prediction[i].className + ": " + prediction[i].probability.toFixed(2);
+    labelContainer.childNodes[i].innerHTML = classPrediction;
+    // don't play audio when head's neutral with probability >= 75%
+    console.log(prediction[0].probability.toFixed(2));
+    if (prediction[0].probability.toFixed(2) >= 0.90 || prediction[0].probability.toFixed(2) >= 0.90)
+      playAud();
+
+    else
+      pauseAud();
   }
 
-  // run the webcam image through the image model
-  async function predict() {
-      // predict can take in an image, video or canvas html element
-      // console.log('Taking a break...');
-    // await sleep(4000);
-    // console.log('Two seconds later, showing sleep in a loop...');
-      const prediction = await model.predict(webcam.canvas);
-      for (let i = 0; i < maxPredictions; i++) {
-          const classPrediction =
-            prediction[i].className + ": " + prediction[i].probability.toFixed(2);
-            labelContainer.childNodes[i].innerHTML = classPrediction;
-       // don't play audio when head's neutral with probability >= 75%
-            console.log(prediction[0].probability.toFixed(2));
-           if (prediction[0].probability.toFixed(2) >= 0.90 || prediction[0].probability.toFixed(2) >= 0.90)
-                playAud();
-               
-           else
-               pauseAud();
-           }
-
-      }
+}
 
 
-  
+
 
 async function stop() {
   location.reload();
-    // await webcam.stop();
-    // document.getElementById("webcam-container").removeChild(webcam.canvas);
-    // document.getElementById("label-container").innerHTML =  "";
-    
+  // await webcam.stop();
+  // document.getElementById("webcam-container").removeChild(webcam.canvas);
+  // document.getElementById("label-container").innerHTML =  "";
+
 }
